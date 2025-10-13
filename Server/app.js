74012f6d94567cb10240a61e0de4c57632edb209
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-10-13 16:34:48 -0300

    Package Json

--- Server/app.js
+++ Server/app.js
@@ -1,3 +1,18 @@
 const express= require('express')
 
 const App= express()
+
+const cors= require('cors')
+
+App.use(cors())
+App.use(express.json())
+
+
+require('dotenv').config()
+const PORT=process.env.PORT ||5000
+
+App.listen(PORT,()=>
+{
+    console.log(`🚀 http://localhost:${PORT}`)
+})
+
