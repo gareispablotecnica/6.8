2bf79cfaa9b7c93747314b76b4cfe2207411afc4
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-10-13 16:55:40 -0300

    zxcz

--- Client/src/Components/Layouts.jsx
+++ Client/src/Components/Layouts.jsx
@@ -3,14 +3,33 @@ import { useState } from 'react'
 import './Layouts.css'
 
 function Layouts() {
-    
+
     const[Usuario,setUsuario]=useState("")
     const[Contraseña,setContraseña]=useState("")
     const[Nombre,setNombre]=useState("")
 
+    const handleSubmit=async()=>{
+
+    }
   return (
     <>
+        <form onSubmit={handleSubmit}>
 
+            <label htmlFor="">Usuario</label>
+            <input type="text" name="Usuario" id="" 
+            value={Usuario}/>
+
+            <label htmlFor="">Contraseña</label>
+            <input type="text" name="Contraseña" id="" 
+            value={Contraseña}/>
+
+            <label htmlFor="">Nombre</label>
+            <input type="text" name="Nombre" id="" 
+            value={Nombre}/>
+
+            <input type="submit" value="Registrar" />
+
+        </form>
     </>
   )
 }
