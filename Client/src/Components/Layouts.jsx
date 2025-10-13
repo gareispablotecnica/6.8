6842499b55152b87320218aeb443c4fc96a3a460
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-10-13 16:46:14 -0300

    Front

---
+++ Client/src/Components/Layouts.jsx
@@ -0,0 +1,18 @@
+// --> rfec + Enter
+import { useState } from 'react'
+import './Layouts.css'
+
+function Layouts() {
+    
+    const[Usuario,setUsuario]=useState("")
+    const[Contraseña,setContraseña]=useState("")
+    const[Nombre,setNombre]=useState("")
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Layouts
