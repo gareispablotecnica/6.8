8c61ffefa5a7b1d0ddf1b1bb18cc34cf7823a575
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-10-13 15:29:29 -0300

    DB

--- Server/src/DataBase/db.js
+++ Server/src/DataBase/db.js
@@ -6,3 +6,21 @@ const path= require('path')
 
 // ---> Ubicación de la Base de Datos
 const dbUbicacion= path.relative(__dirname,'./Artista.db')
+
+const db= new sqlite3.Database(dbUbicacion,(Error)=>{
+    if(Error){
+        console.error('No se Pudo Crear la Base de Datos ⛔')
+    }
+    else{
+        console.log('Base de Datos Creada ✅')
+        db.run(
+            `
+            CREATE TABLE IF NOT EXISTS User(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Usuario TEXT UNIQUE NOT NULL,
+            Contraseña TEXT,
+            Nombre TEXT
+            )`
+        )
+    }
+})
