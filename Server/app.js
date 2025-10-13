b11b4e12c44ed901c1cf90a08f77f575b7b02f64
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-10-13 16:37:35 -0300

    Registrar

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,9 @@ const cors= require('cors')
 App.use(cors())
 App.use(express.json())
 
+const Ruta= require('./src/Routers/LoginRouter')
+// --> Ruta Padre
+App.use('/api',Ruta)
 
 require('dotenv').config()
 const PORT=process.env.PORT ||5000
